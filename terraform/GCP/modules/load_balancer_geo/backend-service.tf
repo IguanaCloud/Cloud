25439659a44210b3bdbbc05a41e519d54578b79e
Yura11 <y.belichenko007@gmail.com>
2024-07-01 21:06:49 +0300

    regional group manager

--- terraform/GCP/modules/load_balancer_geo/backend-service.tf
+++ terraform/GCP/modules/load_balancer_geo/backend-service.tf
@@ -8,7 +8,7 @@ resource "google_compute_region_backend_service" "geo" {
   health_checks         = [google_compute_region_health_check.geo.id]
 
   backend {
-    group           = google_compute_instance_group_manager.geo.instance_group
+    group           = google_compute_region_instance_group_manager.geo.instance_group
     balancing_mode  = "UTILIZATION"
     capacity_scaler = 1.0
   }
