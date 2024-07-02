12ac1ab0927c2cb4be9c5d8c2ad6a7f0d3d7d53e
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-07-02 20:44:09 +0300

    Update load_balancer.tf

--- terraform/GCP/modules/load_balancer_geo/load_balancer.tf
+++ terraform/GCP/modules/load_balancer_geo/load_balancer.tf
@@ -9,37 +9,43 @@ resource "google_compute_subnetwork" "proxy_subnet" {
 }
 
 resource "google_compute_forwarding_rule" "geo" {
-  project               = var.project
   name                  = "${var.env}-${var.region}-${var.app}-geocitizen-forwarding-rule"
-  region                = var.region
   ip_protocol           = "TCP"
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_region_target_http_proxy.geo.id
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "443"
+  target                = google_compute_region_target_https_proxy.geo.id
   network               = var.vpc_network
-  subnetwork            = var.sub_network
   ip_address            = google_compute_address.load_balancer.address
-  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  network_tier          = "STANDARD"
 }
 
-resource "google_compute_region_target_http_proxy" "geo" {
-  project = var.project
-  name    = "${var.env}-${var.region}-${var.app}-geocitizen-proxy"
-  url_map = google_compute_region_url_map.geo.id
-  region  = var.region
+resource "google_compute_region_target_https_proxy" "geo" {
+  name             = "${var.env}-${var.region}-${var.app}-geocitizen-proxy"
+  url_map          = google_compute_region_url_map.geo.id
+  ssl_certificates = [google_compute_region_ssl_certificate.iguana.id]
 }
 
 resource "google_compute_region_url_map" "geo" {
-  project         = var.project
   name            = "${var.env}-${var.region}-${var.app}-geocitizen-map"
-  region          = var.region
   default_service = google_compute_region_backend_service.geo.id
 }
 
 resource "google_compute_address" "load_balancer" {
-  project      = var.project
   name         = "${var.env}-${var.region}-${var.app}-geocitizen-ip-1"
-  address_type = "INTERNAL"
-  region       = var.region
-  subnetwork   = var.sub_network
+  network_tier = "STANDARD"
+}
+
+
+resource "google_compute_region_ssl_certificate" "iguana" {
+  name        = "${var.env}-${var.region}-${var.app}-ssl-certificate-for-iguana"
+  private_key = data.google_secret_manager_secret_version.private_key.secret_data
+  certificate = data.google_secret_manager_secret_version.certificate.secret_data
+}
+
+data "google_secret_manager_secret_version" "private_key" {
+  secret = "privkey"
+}
+
+data "google_secret_manager_secret_version" "certificate" {
+  secret = "fullchain"
 }
