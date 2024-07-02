1525e26c6a0b344f7b94b1762c90b440d6ca5cd7
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-07-02 20:44:34 +0300

    Update template.tf

--- terraform/GCP/modules/load_balancer_geo/template.tf
+++ terraform/GCP/modules/load_balancer_geo/template.tf
@@ -1,22 +1,21 @@
 resource "google_compute_instance_template" "geo_template" {
-  project      = var.project
   name         = "${var.env}-${var.region}-${var.app}-template-geocitizen-webapp"
   machine_type = var.instance_type
-
   network_interface {
     network    = var.vpc_network
     subnetwork = var.sub_network
   }
-
   disk {
     source_image = var.image_type
     auto_delete  = true
     boot         = true
   }
-
   tags = ["allow-ssh", "load-balanced-backend"]
-
   metadata = {
-    ssh-keys = "ubuntu:${file("C:/Users/anton/.ssh/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${data.google_secret_manager_secret_version.ssh_key.secret_data}"
   }
+}
+
+data "google_secret_manager_secret_version" "ssh_key" {
+  secret = "rsa_pub"
 }
