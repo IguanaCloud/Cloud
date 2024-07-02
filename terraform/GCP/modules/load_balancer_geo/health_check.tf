b0e02ca13f3af5e668953a17d7023a32523ad446
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-07-02 20:43:50 +0300

    Update health_check.tf

--- terraform/GCP/modules/load_balancer_geo/health_check.tf
+++ terraform/GCP/modules/load_balancer_geo/health_check.tf
@@ -1,17 +1,6 @@
 resource "google_compute_region_health_check" "geo" {
-  project             = var.project
-  name                = "${var.env}-${var.region}-${var.app}-geocitizen-health-check"
-  region              = var.region
-  check_interval_sec  = 5
-  timeout_sec         = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 10
-
+  name = "${var.env}-${var.region}-${var.app}-geocitizen-hc"
   http_health_check {
-    port = 8080
-  }
-
-  lifecycle {
-    ignore_changes = [name]
+    port_specification = "USE_SERVING_PORT"
   }
 }
