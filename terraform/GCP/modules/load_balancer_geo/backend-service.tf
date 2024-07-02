f039ba2bc55ab193b94787d131437af46a1d174f
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-07-02 20:43:11 +0300

    Update backend-service.tf

--- terraform/GCP/modules/load_balancer_geo/backend-service.tf
+++ terraform/GCP/modules/load_balancer_geo/backend-service.tf
@@ -1,15 +1,14 @@
 resource "google_compute_region_backend_service" "geo" {
-  project               = var.project
-  name                  = "${var.env}-${var.region}-${var.app}-geocitizen-backend-service"
-  region                = var.region
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  protocol              = "HTTP"
-  timeout_sec           = 10
-  health_checks         = [google_compute_region_health_check.geo.id]
-
+  load_balancing_scheme = "EXTERNAL_MANAGED"
   backend {
     group           = google_compute_instance_group_manager.geo.instance_group
     balancing_mode  = "UTILIZATION"
     capacity_scaler = 1.0
+    max_utilization = 0.8
   }
+  name        = "${var.env}-${var.region}-${var.app}-geocitizen-backend-service"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  health_checks = [google_compute_region_health_check.geo.id]
+  port_name     = "http"
 }
