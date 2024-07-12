189ec47ff643c86f3f04ce03ebd74ac2bd0ba377
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-07-12 11:37:11 +0300

    Update secret.tf

--- terraform/GCP/modules/helm_gogs/secret.tf
+++ terraform/GCP/modules/helm_gogs/secret.tf
@@ -19,3 +19,30 @@ resource "kubernetes_secret" "gitea" {
     }))
   }
 }
+
+data "google_secret_manager_secret_version" "tls_cert" {
+  secret = "gogs-certs"  
+  version = "latest"
+  project = var.project
+}
+
+data "google_secret_manager_secret_version" "tls_key" {
+  project = var.project
+  secret = "gogs-key"  
+  version = "latest"
+}
+
+resource "kubernetes_secret" "gogs_https_cert_key" {
+  metadata {
+    name = "gogs-tls"
+    namespace = kubernetes_namespace.gitea.metadata[0].name
+  }
+
+  data = {
+    "tls.crt" = data.google_secret_manager_secret_version.tls_cert.secret_data
+    "tls.key" = data.google_secret_manager_secret_version.tls_key.secret_data
+  }
+
+  type = "kubernetes.io/tls"
+}
+
